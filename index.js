173da87b84ff933089c6285f8d831dd7dc51be89
w9mbui <natalyitotia@gmail.com>
2025-08-06 13:26:23 +0300

    java

--- index.js
+++ index.js
@@ -6,5 +6,27 @@ const form = document.getElementById('transaction-form');
 const textInput = document.getElementById('text');
 const amountInput = document.getElementById('amount');
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const text = textInput.value.trim();
+  const amount = +amountInput.value;
+  if (text === "" || isNaN(amount) || amount <= 0){
+    alert("Please Enter A Valid Amount");
+    return;
+  }
+  
+  const transaction = {
+    id: Date.now(),
+    text,
+    amount
+  };
+  transactions.push(transaction);
+  updateUI();
+  textInput.value = "";
+  amountInput.value = "";
 
+}); 
+function updateUI(){
+    
+}
 
